49927b8c1fc36f071b59ca74d77dc995a733aaa7
AmSmo <adam.smolenski@gmail.com>
2020-08-10 13:55:11 -0400

    spongebobby

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,54 @@
 class Instructor
+    attr_reader :name
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def passed_students
+        passing_tests.map {|tests| tests.student }
+    end
+
+    def passing_tests
+        tests.select {|test| test.test_status == "passed"} 
+    end
+
+    def tests
+        BoatingTest.all.select{|test| test.instructor == self}
+    end
+
+    def all_students
+        tests.map {|test| test.student}
+    end
+
+    def pass_student(student, test_name)
+        grade_student(student, test_name, "pass")
+    end
+    
+    def fail_student(student, test_name)
+        grade_student(student, test_name, "failed")
+    end
+
+    def grade_student(student, test_name, grade)
+        found_test = test_exist?(student, test_name)
+        if found_test
+            found_test.test_status = grade
+        else
+            BoatingTest.new(student, test_name, grade, self)
+        end
+    end
+
+    def test_exist?(student, test_name)
+        tests.find { |test| test.student == student && test.test_name == test_name}
+    end
+
+
+    
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,36 @@
 class Student
+    attr_reader :first_name
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.find_student(name)
+        self.all.select {|student| student.first_name == name}
+    end
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def all_instructors
+        tests.map {|test| test.instructor}
+    end
+
+    def tests
+        BoatingTest.all.select {|test| test.student == self }
+    end
+
+    def grade_percentage
+        #return percentage of passed
+        (tests.count {|test| test.test_status == "passed"} * 1.0 / tests.length)
+    end
 
 end
